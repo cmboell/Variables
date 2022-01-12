50a96e0923545e53cd3cb5c825f3a49a0e15aa26
Colby Boell <68253967+cmboell@users.noreply.github.com>
2022-01-12 17:53:51 -0600

    Initial commit

---
+++ variables.py
@@ -0,0 +1,20 @@
+"""
+Program: variables.py
+Author: Colby Boell
+Last date modified: 1/12/2022
+
+The purpose of this program is to set variables and have them print out
+The input is quantity (integer), item (string), and size (float-point number),
+and a constant variable for price.
+The output should display (printed out) all the variables
+"""
+
+# variables
+quantity = 7
+item = "hats"
+size = 8.5
+# constant
+HAT_PRICE = 12.21
+# print commands to print statements
+print(quantity, ' ', item, ' size: ', size)
+print(item + ' are $', HAT_PRICE)
